a56387e1d42ab0472b2849928c4b413c66e21cea
Yuan <yuan.ncse.tw@gmail.com>
2024-05-01 11:36:29 +0800

    Fix Command Injection

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { exec } = require('child_process');
+const { execFile } = require('child_process');
 const app = express();
 
 app.get('/route4', (req, res) => {
   const query = req.query.req;
   const command = `bgpq3 -4 -j ${query}`;
-
-  exec(command, (error, stdout, stderr) => {
+  const child = execFile('bgpq3', ['-4', '-j', query], (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return res.status(500).send(`Error executing command: ${error}`);
@@ -14,20 +14,20 @@ app.get('/route4', (req, res) => {
 
     res.send(stdout);
   });
+
 });
 
 app.get('/route6', (req, res) => {
   const query = req.query.req;
-  const command = `bgpq3 -6 -j ${query}`;
-
-  exec(command, (error, stdout, stderr) => {
+  const child = execFile('bgpq3', ['-6', '-j', query], (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return res.status(500).send(`Error executing command: ${error}`);
     }
-
+    
     res.send(stdout);
   });
+
 });
 
 const port = process.env.PORT || 6999;
